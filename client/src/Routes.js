691f528536e99c578c44475d205adafd6711e2af
pabloPeralta <paps21.gol@gmail.com>
2019-12-12 01:34:00 -0600

    cambio en ruta del perfil

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import SignupContainer from './components/Signup'
 import LoginContainer from './components/Login'
 import Home from './components/Home/Home'
-import ProfileContainer from './components/Profile'
+import ProfileContainer from './components/ProfileStudent'
 import Navbar from './components/Navbar'
 
 function Routes() {
@@ -14,7 +14,8 @@ function Routes() {
         <Route exact path="/" component={Home}/>
         <Route exact path="/signup" component={SignupContainer}/>
         <Route exact path="/login" component={LoginContainer} />
-        <Route exact path="/profile" component={ProfileContainer} />
+        <Route exact path="/student" component={ProfileContainer} />
+        {/* <Route exact path="/investor" component={ProfileConainer}/> //perfil de investor */}
       </Switch>
     </BrowserRouter>
   )
